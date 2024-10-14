b82bed41e08dd8b585c1a9ff77ee315e5c8cae89
Malte Borgmann <malte@borgmann.tech>
2024-10-15 00:10:38 +0800

    Modified jupyter

--- Homework 1/regression p4.py
+++ Homework 1/regression p4.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+
 
 def generate_data(n=10):
     x = np.random.uniform(0, 1, n)
@@ -27,9 +29,6 @@ def trigonometric(x, m):
 def schwartz_criterion(p, n):
     return 1 + p * ((1 - p) ** -1) * np.log(n)
 
-def akaikes_fpe(p):
-    return (1 + p) * ((1 - p) ** -1)
-
 def plot(x, y, prediction, m):
     sorted_pairs = sorted(zip(x, prediction))
     sorted_x, sorted_y = zip(*sorted_pairs)
@@ -41,39 +40,52 @@ def plot(x, y, prediction, m):
     plt.title("Regression mit m: " + str(m))
     plt.show()
 
+def akaikes_fpe(p):
+    return (1 + p) * ((1 - p) ** -1)
 # %%
 def linear_regression_model(df: pd.DataFrame, function):
     x = df.x
     y = df.y
     data_size = x.size
     schwartz = []
-    coefs = []
+    kfold = KFold(shuffle=True)
     for m in range(1, data_size - 1):
-        coefficients = function(x, m)
-        lr = LinearRegression()
-        lr.fit(coefficients, y)
-        prediction = lr.predict(coefficients)
-        mse = mean_squared_error(y, prediction)
-        print("Mse: ",  mse)
-        schwartz.append(schwartz_criterion(m / data_size, data_size) * mse)
-        coefs.append(lr.coef_)
-        # plot(x, y, prediction, m)
+        kfold_result = []
+        for train_split, test_split in kfold.split(x, y):
+            train_x = np.array([x[i] for i in train_split])
+            train_y = np.array([y[i] for i in train_split])
+            test_x = np.array([[x[i]] for i in test_split])
+            test_y = np.array([y[i] for i in test_split])
+
+            coefficients_train = function(train_x, m)
+            coefficients_test = function(test_x, m)
+
+            lr = LinearRegression()
+            lr.fit(coefficients_train, train_y)
+            prediction = lr.predict(coefficients_test)
+
+            # print("--------------------------------")
+            # print("test - y: ", test_y)
+            # print("Prediction: ", prediction)
+
+            mse = mean_squared_error(test_y, prediction)
+            print("Mse: ", mse)
+            kfold_result.append(schwartz_criterion(m / data_size, data_size) * mse)
+
+        schwartz.append(np.mean(kfold_result))
 
     min_m = np.argmin(schwartz)
+    print("Schwartz: ", schwartz)
 
     print("==============================================")
     print("Result for function:", function.__name__)
     print("Best m: " + str(min_m + 1))
     print("Estimation of: " + str(schwartz[min_m]))
-    print("Coefs: " + str(coefs[min_m]))
 
 
 if __name__ == '__main__':
-    data = generate_data(20)
+    data = generate_data(100)
     linear_regression_model(data, algebraic)
-    #linear_regression_model(data, trigonometric)
+    linear_regression_model(data, trigonometric)
 
-# Je höher die Sample size ist, desto eher wird sich dem Trend angenähert. Folglich sinkt komplexität
-# Nein ist nicht möglich, da wir nur die besten Dimensionen des jeweiligen Modells zurückgegeben werden. Es findet jedoch keine Evaluierung darüber statt,
-# welches der beiden Modelle besser ist. Ein Vergleich dieser wäre möglich, wird in unserer Modelselection jedoch nicht beachtet
-
+# Durch hohe m's haben wir extremes overfitting. es gibt daher immer extreme ausreißer. Das hat zur Folge, dass kleine m das ganze besser darstellen.
